1bb8435db2866ea48592e74500a7aed71ee23098
Jacob Angel <jacobryanangel@gmail.com>
2021-08-28 00:42:12 -0500

    Bot now tts's 'Isaac Dumitru is a stinky poopy stupid head' when Isaac Dumitru enters a voice channel.

--- bot.py
+++ bot.py
@@ -1,4 +1,7 @@
+import asyncio
+from asyncio.windows_events import NULL
 import os
+from discord import channel
 
 from pymongo import MongoClient
 
@@ -20,7 +23,12 @@ DATABASE = os.getenv('DATABASE')
 cluster = MongoClient(os.getenv('CONNECTION_URL'))
 db = cluster[DATABASE]
 
-bot = commands.Bot(command_prefix=os.getenv('COMMAND_PREFIX'), help_command=None)
+bot = commands.Bot(command_prefix=os.getenv(
+    'COMMAND_PREFIX'), help_command=None)
+
+isaacID = 234450128008642571
+pizzaManID = 535610244894556209
+generalChannelID = 297137345730314242
 
 USERS = []
 
@@ -37,7 +45,8 @@ def get_usd_for_symbol(symbol):
     ticker = symbol + '/USDT'
     if ticker in exchange.markets.keys():
         ticker = exchange.fetch_ticker(ticker)
-        usd = (float(ticker['info']['askPrice']) + float(ticker['info']['bidPrice'])) / 2
+        usd = (float(ticker['info']['askPrice']) +
+               float(ticker['info']['bidPrice'])) / 2
         change = float(ticker['info']['priceChangePercent'])
     else:
         usd = None
@@ -60,8 +69,10 @@ def get_usd_for_symbol(symbol):
             data = json.loads(response.text)
             print(data)
             if data.get('status').get('error_message') is None:
-                usd = data.get('data').get(symbol.upper()).get('quote').get('USD').get('price')
-                change = data.get('data').get(symbol.upper()).get('quote').get('USD').get('percent_change_24h')
+                usd = data.get('data').get(symbol.upper()).get(
+                    'quote').get('USD').get('price')
+                change = data.get('data').get(symbol.upper()).get(
+                    'quote').get('USD').get('percent_change_24h')
         except (ConnectionError, Timeout, TooManyRedirects) as e:
             print(e)
 
@@ -81,7 +92,8 @@ async def price(ctx, symbol):
     print('Got price command from: ' + ctx.author.name)
     usd, change = get_usd_for_symbol(symbol)
     if usd is not None:
-        change_formatted = ("{:.1f}".format(change), "+" + "{:.1f}".format(change))[change > 0]
+        change_formatted = ("{:.1f}".format(change), "+" +
+                            "{:.1f}".format(change))[change > 0]
         await ctx.send(f"{symbol} price: ${str(usd)} ({change_formatted}%)")
     else:
         await ctx.send(f"Could not find crypto with symbol {symbol}")
@@ -111,19 +123,23 @@ async def buy(ctx, symbol, amount):
 
             # increase coins
             owned = db["owned"]
-            print(f"Checking for entry in {owned.name} for user {ctx.author.id}, symbol {symbol}")
+            print(
+                f"Checking for entry in {owned.name} for user {ctx.author.id}, symbol {symbol}")
             query = {"user_id": ctx.author.id, "symbol": symbol}
             result = owned.find_one(query)
             number_of_coins = get_number_of_coins(symbol, total)
             if result is None:
-                post = {"user_id": ctx.author.id, "symbol": symbol, "amount": number_of_coins}
+                post = {"user_id": ctx.author.id,
+                        "symbol": symbol, "amount": number_of_coins}
                 owned.insert_one(post)
-                print(f"Added new record for user {ctx.author.id}, symbol {symbol}, amount {number_of_coins}")
+                print(
+                    f"Added new record for user {ctx.author.id}, symbol {symbol}, amount {number_of_coins}")
             else:
                 new_coin_total = result['amount'] + number_of_coins
                 update = {"$set": {"amount": new_coin_total}}
                 owned.update_one(query, update)
-                print(f"Updated record for user {ctx.author.id}, symbol {symbol}, amount {new_coin_total}")
+                print(
+                    f"Updated record for user {ctx.author.id}, symbol {symbol}, amount {new_coin_total}")
 
             balance_formatted = "{:.2f}".format(new_balance_total)
             amount_formatted = "{:.2f}".format(total)
@@ -157,7 +173,8 @@ async def sell(ctx, symbol, amount):
                 new_coin_total = result['amount'] - coins
                 update = {"$set": {"amount": new_coin_total}}
                 owned.update_one(query, update)
-                print(f"Updated owned record for user {ctx.author.id}, symbol {symbol}, amount {new_coin_total}")
+                print(
+                    f"Updated owned record for user {ctx.author.id}, symbol {symbol}, amount {new_coin_total}")
 
                 # increase user balance
                 users = db['users']
@@ -166,9 +183,11 @@ async def sell(ctx, symbol, amount):
                 new_balance_total = this_user['balance'] + price_of_coins
                 update = {"$set": {'balance': new_balance_total}}
                 users.update_one(query, update)
-                print(f"Updated users record for user {ctx.author.id} balance {new_balance_total}")
+                print(
+                    f"Updated users record for user {ctx.author.id} balance {new_balance_total}")
 
-                new_balance_total_formatted = "{:.2f}".format(new_balance_total)
+                new_balance_total_formatted = "{:.2f}".format(
+                    new_balance_total)
                 await ctx.send(f"{ctx.author.name} successfully sold {coins} {symbol}.\n"
                                f"Current {symbol} balance: {new_coin_total} coins.\n"
                                f"Current cash balance: ${new_balance_total_formatted}")
@@ -186,7 +205,8 @@ async def balance(ctx, symbol=None):
         this_user = users.find_one(query)
         if this_user is None:
             print(f"Attempting to add user {ctx.author.id} to DB")
-            post = {"_id": ctx.author.id, "name": ctx.author.name, 'balance': 5000}
+            post = {"_id": ctx.author.id,
+                    "name": ctx.author.name, 'balance': 5000}
             users.insert_one(post)
             print(f"Added user to DB: {post}")
             await ctx.send(f"{ctx.author.name} has joined crypto paper trading! Your cash balance is $5000.00")
@@ -199,7 +219,8 @@ async def balance(ctx, symbol=None):
             total = 0
             for crypto in owned.find({'user_id': ctx.author.id}, {'_id': 0, 'symbol': 1, 'amount': 1}):
                 if crypto['amount'] != 0:
-                    value = crypto['amount'] * get_usd_for_symbol(crypto['symbol'])[0]
+                    value = crypto['amount'] * \
+                        get_usd_for_symbol(crypto['symbol'])[0]
                     total += value
                     value_formatted = "{:.2f}".format(value)
                     cryptos += f"{crypto['symbol']}: ${value_formatted}\n"
@@ -242,7 +263,8 @@ async def leaderboard(ctx):
                 found_price[crypto['symbol']] = crypto_price
                 total += crypto_price * crypto['amount']
         all_users[this_user['name']] = this_user['balance'] + total
-    sorted_users = sorted(all_users.items(), key=lambda kv: kv[1], reverse=True)
+    sorted_users = sorted(
+        all_users.items(), key=lambda kv: kv[1], reverse=True)
     print(f"Got leaderboard: {sorted_users}")
 
     leaderboard_message = "Current standings:\n"
@@ -273,11 +295,42 @@ async def print_help(ctx):
 
     await ctx.send(help_message)
 
+# @bot.command(name='sell_all')
+# async def sell_all(ctx):
+#     await ctx.send(f"Are you sure you want to sell everything {ctx.author.name}? (y / n)")
+
+#     def check(msg):
+#         return msg.author == ctx.author and msg.channel == ctx.channel and msg.content.lower()
+
+#     try:
+#         msg = await bot.wait_for("message", check=check, timeout=15)
+#         if msg.content.lower() == "y":
+#             await ctx.send("You said yes!")
+#             #TODO Code the shit you idiot
+#         else:
+#             await ctx.send("You did not say yes!")
+#     except asyncio.TimeoutError:
+#         await ctx.send("You took too long you fucking idiot.", tts=True)
+
+
+@bot.event
+async def on_voice_state_update(member, before, after):
+    # Get general channel id
+    channel = bot.get_channel(generalChannelID)
+    try:
+        if after.channel.members is not None:
+            # Check if isaac joined a voice channel
+            if member.id == (isaacID):
+                # Say mean things
+                await channel.send("Isaac Dumitru is a stinky poopy stupid head.", tts=True)
+    except Exception:
+        # Need to check if after.members exisits but this works fine for now
+        print("idk it works tho")
+
 
 @bot.event
 async def on_message(message):
     # Check if this message is a command, and if it is evaluate it
     await bot.process_commands(message)
-
 
 bot.run(TOKEN)
